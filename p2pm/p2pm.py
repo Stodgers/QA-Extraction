c1d32fc90932068372db2fa0d2139d8ecb9c5a14
Stodgers <stodgersm@gmail.com>
2019-04-09 15:07:19 +0800

    p2pm

--- p2pm/p2pm.py
+++ p2pm/p2pm.py
@@ -97,12 +97,22 @@ def dic_add(dic_word_path):
         # print(t)
         filter_word.append(t)
         jieba.add_word(t)
-
 dic_add(dic_word_path)
+
+def data_loader(csv_path):
+    ts = open(csv_path, errors='ignore')
+    ts = pd.read_csv(ts).values.tolist()
+    return ts
+
+temp_data_loader = data_loader(csv_path)
+print("temp_data_loader: ",len(temp_data_loader))
 
 def text_filter(temp):
     tec = []
+    k = 0
     for i in temp:
+
+        k+=1
         text_i = str(i[0]).lower()
         text_i = text_i.replace(' ', '')
         text_i = text_i.replace('　', '')
@@ -122,7 +132,7 @@ def text_filter(temp):
                 flag = 1
                 break
         if flag: continue
-        tec.append((ticc, i[1]))
+        tec.append([ticc,i[1],k])
     return tec
 
 text_filter_temp = text_filter(temp_data_loader)  # 86223 len(temp)
@@ -130,8 +140,10 @@ print("text_filter_temp",len(text_filter_temp))
 
 def text_calc_merge(temp):
     tec = []
+    dic_index = {}
     dic = {}
     dic_calc = {}
+    k = 0
     for i in temp:
         Q = i[0]
         Q = Q.replace(' ', '')
@@ -139,23 +151,32 @@ def text_calc_merge(temp):
         A = i[1]
         if Q not in dic:
             dic[Q] = A
+            dic_index[k] = i[2]
+            k += 1
         if Q not in dic_calc:
             dic_calc[Q] = 1
         else:
             dic_calc[Q] += 1
-    tec = [(k, v) for k, v in dic.items()]
-    tec = sorted(tec, key=lambda x: x[0],reverse=True)
-    tec_calc = [(k, v) for k, v in dic_calc.items()]
+
+
+    tec = [[k, v] for k, v in dic.items()]
+    tec_calc = [[k, v] for k, v in dic_calc.items()]
+    for i,v in enumerate(tec):
+        tec_calc[i].append(tec[i][1])
+        tec_calc[i].append(dic_index[i])
+    tec = sorted(tec, key=lambda x: x[0], reverse=True)
     tec_calc = sorted(tec_calc, key=lambda x: x[1],reverse=True)
     return tec, tec_calc
-
 text_calc_merge_temp, temp_calc = text_calc_merge(text_filter_temp)
-
-df_temp_calc = pd.DataFrame(temp_calc)
-df_temp_calc.to_csv('ans//Qrank.csv',index=False,header=False,encoding='utf-8-sig')
+#df_temp_calc = pd.DataFrame(temp_calc)
+#df_temp_calc.to_csv('ans//Qrank.csv',index=False,header=False,encoding='utf-8-sig')
 print("text_calc_merge_temp",len(text_calc_merge_temp))
-
-def word_process_sim(temp):
+print(temp_calc[:2])
+'''
+tec_calc
+q num a index
+'''
+def word_process_sim(temp,temp_calc):
     cilin = SimCilin()
     hownet = SimHownet()
     simhash = SimHaming()
@@ -164,7 +185,13 @@ def word_process_sim(temp):
     len_temp = len(temp)
     ans_temp = []
     k = 0
+    for i,v in enumerate(temp_calc):
+        temp_calc[i].append([])
+
+    #temp_calc = [[i,[]] for i in temp_calc]
+    print(len(temp_calc[0]))
     for i in range(len_temp - 1):
+        temp_q_list = temp_calc[i][4]
         flag = 0
         tex = temp[i][0]
         if i + 10 < len_temp:
@@ -175,10 +202,18 @@ def word_process_sim(temp):
             texj = temp[j][0]
             try:
                 if cilin.distance(tex, texj) >= 0.9:
+                    temp_calc[j][1] += temp_calc[i][1]
+                    temp_calc[i][1] = 0
                     flag = 1
+                    temp_q_list.append(tex)
+                    temp_calc[j][4]+=temp_q_list
                     break
                 if simtoken.distance(tex, texj) >= 0.9:
+                    temp_calc[j][1] += temp_calc[i][1]
+                    temp_calc[i][1] = 0
                     flag = 1
+                    temp_q_list.append(tex)
+                    temp_calc[j][4]+=temp_q_list
                     break
             except Exception:
                 pass
@@ -190,11 +225,35 @@ def word_process_sim(temp):
                 print("{:.2f}%".format(t_jd) + " " + str(k) + " " + str(i), end='\r')
                 #print("{:.2f}%".format(100 * i / (len_temp - 1)) + " " + str(k) + " " + str(i), end='\r')
             k += 1
-    return ans_temp
+    temp_calc = sorted(temp_calc,key=lambda x:x[1],reverse=True)
+    temp_calc = [[i[0],i[2],i[1],i[3],i[4]] for i in temp_calc if i[1]!=0]
+    return ans_temp,temp_calc
 
 ts_ans = []
-ts_ans = word_process_sim(text_calc_merge_temp)
+ts_ans, Qrank= word_process_sim(text_calc_merge_temp,temp_calc)
 print("ts_ans",len(ts_ans), '\n')
 
-cl = cluster(10, ts_ans)
-cl.disp()
+cl = cluster(10, Qrank)
+keyword, rank_ans= cl.disp()
+# for i in Qrank:
+#     print(i)
+rank_temp = []
+cu_temp = []
+sum = 0
+for i,v in enumerate(rank_ans):
+    if i==len(rank_ans)-1 or rank_ans[i][0]!=rank_ans[i+1][0] :
+        cu_temp.append(rank_ans[i])
+        sum += rank_ans[i][4]
+        rank_temp.append([sorted(cu_temp,key=lambda x:x[4],reverse=True),sum])
+        sum=0
+        cu_temp = []
+    else:
+        cu_temp.append(rank_ans[i])
+        sum += rank_ans[i][4]
+
+rank_temp = sorted(rank_temp,key=lambda x:x[1],reverse=True)
+ans = []
+for i in rank_temp:
+    for j in i[0]:
+        ans.append(j)
+        print(j)
